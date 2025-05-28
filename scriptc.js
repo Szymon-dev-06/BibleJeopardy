3c599c0371e2efa73b5a1096f245a3bee9eed876
MacBook air_Szymek <szymek@MacBook-Air-Szymek.local>
2025-05-28 20:27:45 +0200

    new link

--- scriptc.js
+++ scriptc.js
@@ -1,78 +1,78 @@
-const preloader = document.getElementById('preloader');
-const gameBox = document.getElementById('game-box');
-const showQuestion = document.getElementById('showQuestion');
-const trueButton = document.getElementById('true-button');
-const closeButton = document.getElementById('close-button');
-const questionText = document.getElementById('question-text');
-const answerButton = document.getElementById('answer-button');
-const answerText = document.getElementById('answer-text');
+const preloader = document.getElementById('preloader')
+const gameBox = document.getElementById('game-box')
+const showQuestion = document.getElementById('showQuestion')
+const trueButton = document.getElementById('true-button')
+const closeButton = document.getElementById('close-button')
+const questionText = document.getElementById('question-text')
+const answerButton = document.getElementById('answer-button')
+const answerText = document.getElementById('answer-text')
 
 preloader.addEventListener('animationend', function () {
-	this.remove();
-	gameBox.classList.remove('hidden');
-});
+	this.remove()
+	gameBox.classList.remove('hidden')
+})
 
-let questionsDatabase = null;
+let questionsDatabase = null
 
 fetch('questions.JSON')
 	.then(response => response.json())
 	.then(json => {
-		questionsDatabase = json;
-	});
+		questionsDatabase = json
+	})
 
-let questionIndices = {}; 
-let answerIndices = {};
+let questionIndices = {}
+let answerIndices = {}
 
 function questionClick(categoryIndex, level) {
 	if (!(categoryIndex in questionIndices)) {
-		questionIndices[categoryIndex] = {};
+		questionIndices[categoryIndex] = {}
 	}
 	if (!(level in questionIndices[categoryIndex])) {
-		questionIndices[categoryIndex][level] = 0;
+		questionIndices[categoryIndex][level] = 0
 	}
 
-	let questionIndex = questionIndices[categoryIndex][level]; 
-	let questions = questionsDatabase.categories[categoryIndex].questions[level];
+	let questionIndex = questionIndices[categoryIndex][level]
+	let questions = questionsDatabase.categories[categoryIndex].questions[level]
 
-	let question = `Вопрос: ${questions[questionIndex].question}`;
-	questionText.textContent = question;
+	let question = `Вопрос: ${questions[questionIndex].question}`
+	questionText.textContent = question
 
-	showQuestion.dataset.category = categoryIndex;
-	showQuestion.dataset.level = level;
+	showQuestion.dataset.category = categoryIndex
+	showQuestion.dataset.level = level
 
-	answerText.textContent = '';
+	answerText.textContent = ''
 
-	showQuestion.classList.remove('hidden');
+	showQuestion.classList.remove('hidden')
 }
 
 answerButton.addEventListener('click', () => {
-	const categoryIndex = parseInt(showQuestion.dataset.category);
-	const level = parseInt(showQuestion.dataset.level);
+	const categoryIndex = parseInt(showQuestion.dataset.category)
+	const level = parseInt(showQuestion.dataset.level)
 
-	let questionIndex = questionIndices[categoryIndex][level];
-	let questions = questionsDatabase.categories[categoryIndex].questions[level];
-	let answer = `Ответ: ${questions[questionIndex].answer}`;
+	let questionIndex = questionIndices[categoryIndex][level]
+	let questions = questionsDatabase.categories[categoryIndex].questions[level]
+	let answer = `Ответ: ${questions[questionIndex].answer}`
 
-	answerText.textContent = answer;
-});
+	answerText.textContent = answer
+})
 
 function closeModal() {
-	showQuestion.classList.add('hidden');
-	answerText.textContent = '';
+	showQuestion.classList.add('hidden')
+	answerText.textContent = ''
 }
 
-closeButton.addEventListener('click', closeModal);
+closeButton.addEventListener('click', closeModal)
 
 trueButton.addEventListener('click', function () {
-	closeModal();
+	closeModal()
 
-	const categoryIndex = parseInt(showQuestion.dataset.category);
-	const level = parseInt(showQuestion.dataset.level);
+	const categoryIndex = parseInt(showQuestion.dataset.category)
+	const level = parseInt(showQuestion.dataset.level)
 
 	if (
 		questionIndices[categoryIndex] &&
 		questionIndices[categoryIndex][level] !== undefined
 	) {
-		questionIndices[categoryIndex][level]++;
+		questionIndices[categoryIndex][level]++
 	}
-});
+})
